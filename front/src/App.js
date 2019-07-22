c90c6e370fd59a3b0d5140157bd1cd264b97b724
Lea-jacta-est <lea.godard.web@gmail.com>
2019-07-22 21:15:25 +0200

    merge de paginationTRUC, chatbox et profiledetails

--- front/src/App.js
+++ front/src/App.js
@@ -1,35 +1,21 @@
-<<<<<<< HEAD
-import React from 'react';
-import Footer from './components/Footer';
-import Home from './components/Home';
-import {
-  Switch,
-  Route
-} from 'react-router-dom';
-import HeroProfil from './components/HeroProfil';
-import Navbar from './components/Navbar';
-
-=======
 import React from "react";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import { Switch, Route } from "react-router-dom";
+import HeroProfil from "./components/HeroProfil";
+import Navbar from "./components/Navbar";
 import Chatbot from "./components/Chatbot";
->>>>>>> chatbox
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/profil-hero/:id" component={HeroProfil} />
       </Switch>
-=======
       <Home />
-
       <Chatbot />
->>>>>>> chatbox
       <Footer />
     </div>
   );
