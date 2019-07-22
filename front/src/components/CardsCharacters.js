ac58e7b02d95798a6eed6af4c179e86d05615574
Sandy Prolhac <tottati@hotmail.com>
2019-07-23 00:21:29 +0200

    cards bien rangées, menu sidebar aligné

--- front/src/components/CardsCharacters.js
+++ front/src/components/CardsCharacters.js
@@ -7,39 +7,41 @@ import { withRouter } from "react-router";
 const CardsCharacters = ({ content, gender, hair, eye, search, id }) => (
 
   <Container style={{ marginTop: '4.5rem' }}>
-    <Grid>
+    <Grid columns={3}>
       {content && content
         .filter(character => character.gender.includes(gender))
         .filter(character => character.hairColor.includes(hair))
         .filter(character => character.eyeColor.includes(eye))
         .filter(character => character.name.toLowerCase().includes(search))
         .map((character) =>
-          <Card key={character.id}>
-            <Link
-              to={`/profil-hero/${character.id}`}
-            >
-              <Image src={character.imageMd} wrapped ui={false} />
-            </Link>
-            <Card.Content >
-              <Card.Header>{character.name}</Card.Header>
-              <Card.Meta>
-                <span className='date'> {character.placeOfBirth}</span>
-              </Card.Meta>
-              <Card.Description>
-                <List>
-                  <List.Item>
-                    <Icon disabled name='non binary transgender' /> {character.gender}
-                  </List.Item>
-                  <List.Item>
-                    <Icon disabled name={character.race ? 'male' : " "} />  {character.race}
-                  </List.Item>
-                  <List.Item>
-                    <Icon disabled name={character.alignment === "good" ? 'smile' : 'frown outline'} /> {character.alignment}
-                  </List.Item>
-                </List>
-              </Card.Description>
-            </Card.Content>
-          </Card>
+          <Grid.Column>
+            <Card key={character.id} wrapped ui={false}>
+              <Link
+                to={`/profil-hero/${character.id}`}
+              >
+                <Image src={character.imageMd} wrapped ui={true} />
+              </Link>
+              <Card.Content >
+                <Card.Header>{character.name}</Card.Header>
+                <Card.Meta>
+                  <span className='date'> {character.placeOfBirth}</span>
+                </Card.Meta>
+                <Card.Description>
+                  <List>
+                    <List.Item>
+                      <Icon disabled name='non binary transgender' /> {character.gender}
+                    </List.Item>
+                    <List.Item>
+                      <Icon disabled name={character.race ? 'male' : " "} />  {character.race}
+                    </List.Item>
+                    <List.Item>
+                      <Icon disabled name={character.alignment === "good" ? 'smile' : 'frown outline'} /> {character.alignment}
+                    </List.Item>
+                  </List>
+                </Card.Description>
+              </Card.Content>
+            </Card>
+          </Grid.Column>
         )}
     </Grid>
 
