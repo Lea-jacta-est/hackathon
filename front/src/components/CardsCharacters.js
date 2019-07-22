1b6a517288101af8fdd938a7a6c3742405bcdee7
TowTowRow <towtowrow@gmail.com>
2019-07-22 14:34:22 +0200

    init

--- front/src/components/CardsCharacters.js
+++ front/src/components/CardsCharacters.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Icon, Image, Container, Grid } from 'semantic-ui-react'
+import { Card, Icon, Image, Container, Grid, List } from 'semantic-ui-react'
 
 
 
@@ -15,16 +15,23 @@ const CardsCharacters = ({ content }) => (
           <Card.Content>
             <Card.Header>{character.name}</Card.Header>
             <Card.Meta>
-              <span className='date'>Joined in 2015</span>
+              <span className='date'> {character.biography.placeOfBirth}</span>
             </Card.Meta>
             <Card.Description>
-              Matthew is a musician living in Nashville.
-                </Card.Description>
+              <List>
+                <List.Item>
+                  <Icon disabled name='non binary transgender' /> {character.appearance.gender}
+                </List.Item>
+                <List.Item>
+                  <Icon disabled name={character.appearance.race ? 'male' : " "} />  {character.appearance.race}
+                </List.Item>
+                <List.Item>
+                  <Icon disabled name={character.biography.alignment === "good" ? 'smile' : 'frown outline'} /> {character.biography.alignment}
+                </List.Item>
+              </List>
+            </Card.Description>
           </Card.Content>
-          <Card.Content extra>
-            <Icon name='user' />
-            22 Friends
-              </Card.Content>
+
         </Card>
       )}
     </Grid>
