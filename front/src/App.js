984aef46190e963cc2b76664c1103bfd78b0700c
sbaboot <sebastienflamant31@gmail.com>
2019-07-22 14:01:25 +0200

    ajout footer

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Home from './components/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-      <Home />
+      <Footer />
     </div>
   );
 }
