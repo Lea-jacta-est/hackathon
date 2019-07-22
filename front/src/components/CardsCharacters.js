4c35f69e3d59c356247c580ed30b39e8e223bf95
Sandy Prolhac <tottati@hotmail.com>
2019-07-22 13:59:04 +0200

    affichage cards, call api

---
+++ front/src/components/CardsCharacters.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Card, Icon, Image, Container, Grid } from 'semantic-ui-react'
+
+
+
+
+
+const CardsCharacters = ({ content }) => (
+
+  <Container>
+    <Grid>
+      {content && content.map(character =>
+        <Card>
+          <Image src={character.images.md} wrapped ui={false} />
+          <Card.Content>
+            <Card.Header>{character.name}</Card.Header>
+            <Card.Meta>
+              <span className='date'>Joined in 2015</span>
+            </Card.Meta>
+            <Card.Description>
+              Matthew is a musician living in Nashville.
+                </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Icon name='user' />
+            22 Friends
+              </Card.Content>
+        </Card>
+      )}
+    </Grid>
+  </Container>
+);
+
+
+export default CardsCharacters;
---
+++ front/src/components/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import axios from 'axios';
+import CardsCharacters from './CardsCharacters';
+
+
+class Home extends React.Component {
+  state = {
+    content: []
+  }
+
+
+  componentDidMount() {
+    axios.get('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json')
+      .then(resp => (resp.data))
+      .then(data => this.setState({ content: data }))
+  }
+
+
+  render() {
+    const { content } = this.state;
+    return (
+
+      <CardsCharacters content={content} />
+    );
+  }
+}
+
+export default Home;
