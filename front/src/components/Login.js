574baf24130157dbbbf43830a00e58ab4e122972
TowTowRow <towtowrow@gmail.com>
2019-07-23 14:06:45 +0200

    size bg

--- front/src/components/Login.js
+++ front/src/components/Login.js
@@ -1,39 +1,47 @@
 import React from 'react';
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
+import { Button, Form, Grid, Header, Image, Message, Segment, Container } from 'semantic-ui-react';
 import { Link } from "react-router-dom";
 import logo from './image/logo.png';
+import "../App.css"
 
 const Login = () => (
-  <Grid textAlign='center' style={{ height: '100vh', backgroundColor: 'black' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header as='h2' color='pink' textAlign='center'>
-        <Image src={logo} /> Log-in to your account
+  <Container style={{ backgroundColor: 'black' }} fluid>
+    <div className='bghero' >
+      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+
+        <Grid.Column style={{ maxWidth: 450, backgroundColor: 'black' }}>
+
+          <Header as='h2' color='pink' textAlign='center'>
+            <Image src={logo} /> Log-in to your account
       </Header>
-      <Form size='large'>
-        <Segment stacked>
-          <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-          <Form.Input
-            fluid
-            icon='lock'
-            iconPosition='left'
-            placeholder='Password'
-            type='password'
-          />
-          <Link to="/home">
-            <Button color='pink' fluid size='large'>
-              Login
+          <Form size='large'>
+            <Segment stacked>
+              <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+              <Form.Input
+                fluid
+                icon='lock'
+                iconPosition='left'
+                placeholder='Password'
+                type='password'
+              />
+              <Link to="/home">
+                <Button color='pink' fluid size='large'>
+                  Login
           </Button>
-          </Link>
-        </Segment>
-      </Form>
-      <Message>
-        New to us?
+              </Link>
+            </Segment>
+          </Form>
+          <Message>
+            New to us?
           <Link to="/signup">
-          Sign Up
+              Sign Up
           </Link>
-      </Message>
-    </Grid.Column>
-  </Grid>
+          </Message>
+
+        </Grid.Column>
+      </Grid >
+    </div>
+  </Container >
 )
 
 export default Login;
